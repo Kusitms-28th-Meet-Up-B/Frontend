0a8b3b75836b44ca73d69720d2168e11f84e4f27
aaminha <dks4857@gmail.com>
2023-10-28 23:16:29 +0900

    design: 공통 스타일 요소 inner width 수정

--- src/style/common.tsx
+++ src/style/common.tsx
@@ -1,8 +1,7 @@
 import { styled } from 'styled-components';
 
 export const CommonInner = styled.div`
-  max-width: 1920px;
-  min-width: 1400px;
+  width: 1400px;
   margin: 0 auto;
   padding: 0;
   position: relative;
