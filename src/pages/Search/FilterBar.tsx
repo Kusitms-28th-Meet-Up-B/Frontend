64fcd3ff19d88a04f4fad1fde619ac79cd221a74
aaminha <dks4857@gmail.com>
2023-10-29 03:09:37 +0900

    feat: FilterBar 컴포넌트 구현

---
+++ src/pages/Search/FilterBar.tsx
@@ -0,0 +1,35 @@
+import FilterButton from '@/components/Button/FilterButton';
+import MainButton from '@/components/Button/MainButton';
+import { FILTER_LIST } from '@/constant';
+import { B2Bold } from '@/style/fonts/StyledFonts';
+import styled from 'styled-components';
+
+const FilterBar = () => {
+  return (
+    <Container>
+      <FilterButtonContainer>
+        {FILTER_LIST.map(filter => (
+          <FilterButton filter={filter} />
+        ))}
+      </FilterButtonContainer>
+      <MainButton $buttonSize="small">
+        <B2Bold $fontColor="white">선택</B2Bold>
+      </MainButton>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 34px;
+  padding: 40px 0px;
+  margin-bottom: 64px;
+`;
+
+const FilterButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+`;
+export default FilterBar;
