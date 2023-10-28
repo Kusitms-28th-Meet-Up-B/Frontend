8bf86ef6c6fbc2bd0092c5f54e4a9802f6a4c4c1
aaminha <dks4857@gmail.com>
2023-10-29 00:54:48 +0900

    feat: MainButton 컴포넌트 구현

---
+++ src/components/Button/MainButton.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+interface ButtonStyle {
+  $buttonSize: 'big' | 'small';
+  $buttonWidth?: string;
+}
+
+interface ButtonProps extends ButtonStyle {
+  children: React.ReactNode;
+}
+
+const MainButton = ({ $buttonSize, $buttonWidth, children }: ButtonProps) => {
+  return (
+    <StyledButton $buttonSize={$buttonSize} $buttonWidth={$buttonWidth}>
+      {children}
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button<ButtonStyle>`
+  background-color: var(--color_sub3);
+  border-radius: 10px;
+  padding: ${({ $buttonSize }) =>
+    $buttonSize === 'small' ? '7.5px 40px' : '16px 56px'};
+  width: ${({ $buttonWidth }) => $buttonWidth};
+`;
+
+export default MainButton;
