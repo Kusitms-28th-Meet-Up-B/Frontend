146f0bc954250d3ab5de3c376f07e47d72e6036d
jk6722 <jk6722@hanyang.ac.kr>
2023-10-29 01:29:36 +0900

    chore: 불필요한 import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './pages/main/Main';
 import Map from './pages/map/Map';
 import { GlobalStyle } from './style/GlobalStyle';
