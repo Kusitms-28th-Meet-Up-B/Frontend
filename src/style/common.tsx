d11f013d3c42985674e1bdcff967ca304d3314a8
aaminha <dks4857@gmail.com>
2023-10-28 21:54:36 +0900

    design: 공통 스타일 요소 inner 설정

---
+++ src/style/common.tsx
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const CommonInner = styled.div`
+  max-width: 1920px;
+  min-width: 1400px;
+  margin: 0 auto;
+  padding: 0;
+  position: relative;
+`;
