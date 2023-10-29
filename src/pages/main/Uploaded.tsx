93a4b882bbb844c620d1f5f4ca47397955fbb3f2
jk6722 <jk6722@hanyang.ac.kr>
2023-10-29 15:40:00 +0900

    feat: 최신 업로드 프로그램 (미완성)

---
+++ src/pages/main/Uploaded.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Uploaded = () => {
+  return <div>Uploaded</div>;
+};
+
+export default Uploaded;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ProgramContainer = styled.div``;
