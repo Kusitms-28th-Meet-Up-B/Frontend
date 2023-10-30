8e6ca6c7ad579bd7de34b54607ceaca9d6f3e44b
aaminha <dks4857@gmail.com>
2023-10-30 13:35:02 +0900

    feat(ProgramCardItem): 프로그램 정보를 카드로 보여주는 기능 구현

--- src/components/ProgramCard/ProgramCardItem.tsx
+++ src/components/ProgramCard/ProgramCardItem.tsx
@@ -1,43 +1,52 @@
 import styled from 'styled-components';
-import Poster from '@/assets/temp/poster.jpg';
 import FavoriteIcon from '@/assets/icons/favorite_icon.svg';
 import FavoriteButtonIcon from '@/assets/icons/favorite_button_icon.svg';
 import UnfavoriteButtonIcon from '@/assets/icons/unfavorite_button_icon.svg';
 import { B1Bold, B3, B3Bold, H3 } from '@/style/fonts/StyledFonts';
+import { ProgramMainInfoType } from '@/types';
+import { useState } from 'react';
 
-const tagList = ['#태안', '#태안군', '#지원사업', '#한달살이'];
+const ProgramCardItem = ({ program }: { program: ProgramMainInfoType }) => {
+  // 프로그램 좋아요 했는지 여부 (임시)
+  // TODO: favorite/undfavorite 버튼 클릭할 때마다 서버와 통신?
+  const [favorite, setFavorite] = useState<boolean>(true);
 
-const ProgramCardItem = () => {
+  const handleFavorite = () => {
+    setFavorite(!favorite);
+  };
+
   return (
     <Container>
-      {true ? (
+      {!favorite ? (
         <img
           className="favorite-button"
           alt="favorite-button-icon"
           src={FavoriteButtonIcon}
+          onClick={handleFavorite}
         />
       ) : (
         <img
           className="favorite-button"
           alt="unfavorite-button-icon"
           src={UnfavoriteButtonIcon}
+          onClick={handleFavorite}
         />
       )}
-      <img className="poster" alt="poster" src={Poster} />
+      <img className="poster" alt="program-poster" src={program.photoUrl} />
       <ProgramInfoContainer>
-        <B1Bold $fontColor="var(--color_sub3)">D-1</B1Bold>
-        <H3 $fontColor="var(--color_gray900)">태안 한달살이</H3>
-        <TagContainer>
-          {tagList.map(tag => (
+        <B1Bold $fontColor="var(--color_sub3)">{program.remainDay}</B1Bold>
+        <H3 $fontColor="var(--color_gray900)">{program.programName}</H3>
+        <div className="hashTag-container">
+          {program.hashTag.map(tag => (
             <B3 $fontColor="var(--color_gray400)" key={tag}>
-              {tag}
+              #{tag}
             </B3>
           ))}
-        </TagContainer>
-        <Favorite>
+        </div>
+        <div className="favorite-container">
           <img alt="favorite-icon" src={FavoriteIcon} />
-          <B3Bold $fontColor="var(--color_gray600)">153</B3Bold>
-        </Favorite>
+          <B3Bold $fontColor="var(--color_gray600)">{program.like}</B3Bold>
+        </div>
       </ProgramInfoContainer>
     </Container>
   );
@@ -52,12 +61,14 @@ const Container = styled.div`
     position: absolute;
     right: 17px;
     top: 18px;
+    cursor: pointer;
   }
 
   .poster {
     width: 323px;
     height: 400px;
     border-radius: 20px;
+    //object-fit: cover;
   }
 `;
 
@@ -67,23 +78,23 @@ const ProgramInfoContainer = styled.div`
   align-items: start;
   padding-top: 16px;
   gap: 4px;
-`;
 
-const TagContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  gap: 8px;
-`;
+  .hashTag-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 8px;
+  }
 
-const Favorite = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 4px;
+  .favorite-container {
+    display: flex;
+    flex-direction: row;
+    gap: 4px;
 
-  img {
-    width: 16px;
-    height: 16px;
+    img {
+      width: 16px;
+      height: 16px;
+    }
   }
 `;
 
