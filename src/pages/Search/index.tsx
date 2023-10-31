0eecdfd67b9c8a5fa0f0d7e2ba673075bb1c4f55
aaminha <dks4857@gmail.com>
2023-10-31 20:56:13 +0900

    feat: Search 컴포넌트에 SearchBar 적용

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -3,11 +3,25 @@ import styled from 'styled-components';
 import MapButton from './MapButton';
 import SearchResult from './SearchResult';
 import FilterBar from './FilterBar';
+import SearchBar from '@/components/SearchBar/SearchBar';
+import { useState } from 'react';
 
 const Search = () => {
+  const [searchInput, setSearchInput] = useState<string>('');
   return (
     <CommonInner>
       <Container>
+        <SearchWrapper>
+          <SearchBar
+            placeHolder="어디로 떠나고 싶으신건가요?"
+            searchInput={searchInput}
+            setSearchInput={setSearchInput}
+            handleClick={() => {
+              // TODO: API 연동
+              setSearchInput('');
+            }}
+          />
+        </SearchWrapper>
         <FilterBar />
         <SearchResult />
         <MapButton />
@@ -17,14 +31,16 @@ const Search = () => {
 };
 
 const Container = styled.div`
-  //display: flex;
-  //flex-direction: column;
-  //justify-content: center;
-  //align-items: center;
   text-align: center;
-  //background-color: pink;
   margin-top: 180px;
   margin-bottom: 80px;
+`;
+
+const SearchWrapper = styled.div`
+  display: flex;
+  width: 900px;
+  height: 80px;
+  margin: 0 auto;
 `;
 
 export default Search;
