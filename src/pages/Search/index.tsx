e8c10d81ecf25bad3af1dc1faea52ca1c686a165
aaminha <dks4857@gmail.com>
2023-11-04 20:24:21 +0900

    fix: 검색 후 path값이 변경되도록 수정

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -10,11 +10,10 @@ import ProgramSearchBar from './ProgramSearchBar';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const Search = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const [searchInput, setSearchInput] = useState<string>('');
   const [filterInput, setFilterInput] =
     useState<FilterInputType>(DEFUALT_FILTER_LIST);
-  const [searchPath, setSearchPath] = useState<string>('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -28,22 +27,18 @@ const Search = () => {
 
     setFilterInput(newFilterInput);
 
-    // TODO : Search API로 GET
+    // TODO : Search API로 GET ()
+    // 검색어는 keyword로 필터는 newFilterInput으로
   }, []);
 
-  /* const handleFilterInput = () => {
-    setFilterInput({});
-  }; */
-
   const handleSubmit = () => {
+    let searchPath = '';
+    for (const key in filterInput) {
+      if (filterInput[key]) searchPath += `&${key}=${filterInput[key]}`;
+    }
     navigate(`/search?keyword=${searchInput}${searchPath}`);
+    // TODO: 데이터 다시 GET
   };
-
-  // TODO : 검색 API 연결 (useParams의 type 이용)
-  // useParams의 type 종류 => program(카드로 이동한 경우) / search(검색어로 이동한 경우)
-  // useSearchParams를 이용해서 검색어 및 키워드 보여줄 예정 (ex. /search/program?keyword=여행지원사업)
-  // useSearchParams에서는 keyword는 필수. sort, location, program, 등등은 필요 시 들어갈 예정
-  // handleSubmit 구현해서 SearchBar의 handleSubmit, 선택 버튼 onClick에 적용할 예정
 
   return (
     <CommonInner>
