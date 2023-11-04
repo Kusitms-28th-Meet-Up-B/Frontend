71792fe1f957853a6ad33d1215420b9c41ca34d9
aaminha <dks4857@gmail.com>
2023-11-04 17:00:00 +0900

    fix: 메인에서 NavCard 클릭 시 보이는 문구 수정

--- src/pages/Search/SearchResult.tsx
+++ src/pages/Search/SearchResult.tsx
@@ -1,26 +1,38 @@
 import ProgramCard from '@/components/ProgramCard/ProgramCard';
 import { H1 } from '@/style/fonts/StyledFonts';
-import { ProgramMainInfoType } from '@/types';
+import { FilterInputType, ProgramMainInfoType } from '@/types';
 import styled from 'styled-components';
 
 interface SearchResultProps {
+  type: string | undefined;
   keyword: string;
+  filter: FilterInputType;
   programCount: number;
   programList: ProgramMainInfoType[];
 }
 
 const SearchResult = ({
+  type,
   keyword,
+  filter,
   programCount,
   programList,
 }: SearchResultProps) => {
   return (
     <>
-      <ResultHeader>
-        <H1 $fontColor="var(--color_gray900)">{`'${keyword}' 여행 프로그램`}</H1>
-        <H1 $fontColor="var(--color_sub3)">{` ${programCount}`}</H1>
-        <H1 $fontColor="var(--color_gray900)">{`건`}</H1>
-      </ResultHeader>
+      {type === 'program' ? (
+        <ResultHeader>
+          <H1 $fontColor="var(--color_gray900)">{`전체 ${filter.program}`}</H1>
+          <H1 $fontColor="var(--color_sub3)">{` ${programCount}`}</H1>
+          <H1 $fontColor="var(--color_gray900)">{`건`}</H1>
+        </ResultHeader>
+      ) : (
+        <ResultHeader>
+          <H1 $fontColor="var(--color_gray900)">{`'${keyword}' 여행 프로그램`}</H1>
+          <H1 $fontColor="var(--color_sub3)">{` ${programCount}`}</H1>
+          <H1 $fontColor="var(--color_gray900)">{`건`}</H1>
+        </ResultHeader>
+      )}
       <ProgramCard programList={programList} />
     </>
   );
