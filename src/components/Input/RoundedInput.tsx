90f472f0e8e5cf4df49590312f29714e8249b655
aaminha <dks4857@gmail.com>
2023-11-06 03:54:51 +0900

    feat: RoundedInput 컴포넌트 구현

---
+++ src/components/Input/RoundedInput.tsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+interface RoundedInputProps {
+  placeholder: string;
+  name: string;
+  value: string;
+  onChange: () => void;
+}
+
+const RoundedInput = ({
+  placeholder,
+  name,
+  value,
+  onChange,
+}: RoundedInputProps) => {
+  return (
+    <Wrapper>
+      <input
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  background-color: red;
+
+  input {
+    width: 100%;
+    border-radius: 40px;
+    border: 1px solid var(--color_gray200);
+    padding: 20px 36px;
+
+    color: var(--color_gray900);
+    font-family: SUIT-Medium;
+    font-size: 18px;
+    font-style: normal;
+    line-height: 40%;
+  }
+
+  input::placeholder {
+    color: var(--color_gray400);
+  }
+`;
+export default RoundedInput;
