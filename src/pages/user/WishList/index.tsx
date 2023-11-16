6a2c32ed688ea8b557ab054ad9c9dbe0b3889f1a
aaminha <dks4857@gmail.com>
2023-11-17 03:15:55 +0900

    feat: 찜 목록 페이지 구현

--- src/pages/user/WishList/index.tsx
+++ src/pages/user/WishList/index.tsx
@@ -1,4 +1,64 @@
+import { DEFAULT_WISHLIST_FILTER_LIST } from '@/constants/User/wishlist';
+import { CommonInner } from '@/style/common';
+import { WishlistFilterType } from '@/types';
+import { useState } from 'react';
+import FilterBar from './FilterBar';
+import UserTitle from '@/components/Title/UserTitle';
+import styled from 'styled-components';
+import ProgramCard from '@/components/ProgramCard/ProgramCard';
+import axios from 'axios';
+
 const WishList = () => {
-  return <div>위시</div>;
+  const [filterInput, setFilterInput] = useState<WishlistFilterType>(
+    DEFAULT_WISHLIST_FILTER_LIST,
+  );
+  const [data, setData] = useState(null);
+
+  const handleSubmit = () => {
+    // TODO: 데이터 다시 GET */
+    console.log(filterInput);
+    test();
+  };
+
+  const handleChangeFilterInput = (itemTitle: string, itemInput: string) => {
+    setFilterInput({ ...filterInput, [itemTitle]: itemInput });
+  };
+
+  /////////////////////////
+  const test = () => {
+    axios
+      .get('http://52.78.13.36/programs/recent')
+      .then(response => {
+        console.log(response.data.result);
+        setData(response.data.result);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+  /////////////////////////
+
+  return (
+    <CommonInner>
+      <Container>
+        <UserTitle>갈래 말래</UserTitle>
+        <FilterBar
+          filterInput={filterInput}
+          handleChangeFilterInput={handleChangeFilterInput}
+          handleSubmit={handleSubmit}
+        />
+      </Container>
+      {data && <ProgramCard programList={data} />}
+    </CommonInner>
+  );
 };
 export default WishList;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+  margin-top: 120px;
+  margin-bottom: 180px;
+`;
