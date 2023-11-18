2fdd3566273d38ae4af810c0c006e8a8ee1a83e0
jk6722 <jk6722@hanyang.ac.kr>
2023-11-18 16:16:10 +0900

    feat: 소셜 로그인 컴포넌트 따로 분리

---
+++ src/pages/login/SocialLogin.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+const SocialLogin = () => {
+  const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY; //REST API KEY
+  const REDIRECT_URI = `${window.location.origin}/kakao/login`; //REDIRECT URI
+  // oauth 요청 URL
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const handleKakaoLogin = () => {
+    // 카카오 로그인 주소
+    window.location.href = kakaoURL;
+  };
+
+  return (
+    <SocialLoginContainer>
+      {/* 네이버 로그인 */}
+      <SocialLoginButton>
+        <img src="/src/assets/icons/icon-naver.svg" alt="naver" />
+      </SocialLoginButton>
+      {/* 카카오 로그인 */}
+      <SocialLoginButton onClick={handleKakaoLogin}>
+        <img src="/src/assets/icons/icon-kakao.svg" alt="kakao" />
+      </SocialLoginButton>
+      {/* 구글 로그인 */}
+      <SocialLoginButton>
+        <img src="/src/assets/icons/icon-google.svg" alt="google" />
+      </SocialLoginButton>
+    </SocialLoginContainer>
+  );
+};
+
+export default SocialLogin;
+
+const SocialLoginContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+
+  gap: 16px;
+`;
+
+const SocialLoginButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 56px;
+  height: 56px;
+  flex-shrink: 0;
+
+  background: #f6f6f6;
+  border-radius: 50%;
+
+  cursor: pointer;
+`;
