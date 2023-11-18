d42917fd0f4d8eb83830288794ccb30808deabc9
jk6722 <jk6722@hanyang.ac.kr>
2023-11-19 08:58:43 +0900

    chore: 사용하지 않는 변수 제거

--- src/pages/login/functions/index.ts
+++ src/pages/login/functions/index.ts
@@ -3,6 +3,7 @@ import { AxiosResponse } from 'axios';
 
 export const onLoginSuccess = (res: AxiosResponse) => {
   const { accessToken, refreshToken } = res?.data?.result;
+  refreshToken;
   Axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
 };
 
